4bfe45799608c71c26e96d076057e7614957ada7
MouleFrites <louisarmand.wilfart@ynov.com>
2018-11-25 18:43:44 +0100

    breakingballs

--- WILFART_Louisarmand_kholle_1.py
+++ WILFART_Louisarmand_kholle_1.py
@@ -6,6 +6,7 @@
 
 import signal
 import sys
+import csv
 
 
 def exitproperly(sig, frame):
@@ -15,3 +16,36 @@ def exitproperly(sig, frame):
 signal.signal(signal.SIGINT, exitproperly)
 
 
+fileName = 'liste.csv'
+
+def show():
+
+    with open('liste.csv') as csv_file:
+        read_csv = csv.reader(csv_file)
+        for row in read_csv:
+            print(', '.join(row))
+    csv_file.close()
+
+def add():
+    if len(sys.argv) < 3:
+        print("Faudrais ptete mettre des chiffres nan ?")
+    else:
+        files = open(fileName, "wb")
+        writer = csv.writer(files, delimiter=",")
+        lenArgs = len(sys.argv) - 2
+        for i in range(lenArgs):
+            test = []
+            a = i + 2
+            writer.writerow( (int(sys.argv[a])) )
+
+
+def choice():
+    if len(sys.argv) < 2:
+        print("Faudrais ptete mettre des arguments nan ?")
+    else:
+        if str(sys.argv[1]) == "-l":
+            show()
+        elif str(sys.argv[1]) == "-a":
+            add()
+    
+choice()
