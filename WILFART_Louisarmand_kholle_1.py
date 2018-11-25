1737fd4e9a0cf7d553cae40dfe95da694b44d774
MouleFrites <louisarmand.wilfart@ynov.com>
2018-11-25 23:22:18 +0100

    flm_de_continuer

--- WILFART_Louisarmand_kholle_1.py
+++ WILFART_Louisarmand_kholle_1.py
@@ -7,6 +7,7 @@
 import signal
 import sys
 import csv
+from statistics import mean
 
 
 def exitproperly(sig, frame):
@@ -30,14 +31,44 @@ def add():
     if len(sys.argv) < 3:
         print("Faudrais ptete mettre des chiffres nan ?")
     else:
-        files = open(fileName, "wb")
-        writer = csv.writer(files, delimiter=",")
+        files = open(fileName, "a")
         lenArgs = len(sys.argv) - 2
         for i in range(lenArgs):
-            test = []
             a = i + 2
-            writer.writerow( (int(sys.argv[a])) )
+            if i == 0:
+                tabInsert = '\n' + sys.argv[a] + ','
+            elif i == lenArgs - 1:
+                tabInsert = sys.argv[a]
+            else:
+                tabInsert = sys.argv[a] + ','
+            files.write(tabInsert)
+        files.close()
 
+def rm():
+    remove = open(fileName, "w")
+    remove.write('')
+    remove.close()
+
+def minmaxmoysum():
+    if len(sys.argv) < 3:
+        print("Faudrais ptete mettre des arguments nan ?")
+    else:
+        varTab = []
+        a = 0
+        with open('liste.csv') as csv_file:
+            read_csv = csv.reader(csv_file)
+            for row in read_csv:
+                a += 1
+                varTab.append(row)
+            csv_file.close()
+        if str(sys.argv[2]) == "--min":
+            print(min(varTab))
+        elif str(sys.argv[2]) == "--max":
+            print(max(varTab))
+        elif str(sys.argv[2]) == "--moy":
+            print(mean(varTab))
+        elif str(sys.argv[2]) == "--sum":
+            print(a)
 
 def choice():
     if len(sys.argv) < 2:
@@ -47,5 +78,9 @@ def choice():
             show()
         elif str(sys.argv[1]) == "-a":
             add()
+        elif str(sys.argv[1]) == "-c":
+            rm()
+        elif str(sys.argv[1]) == "-s":
+            minmaxmoysum()
     
 choice()
